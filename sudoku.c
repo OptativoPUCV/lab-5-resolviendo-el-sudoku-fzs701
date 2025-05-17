3d11799485e804002b3948e52564fbf3217018da
Your Name <you@example.com>
2025-05-16 22:11:48 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -51,11 +51,10 @@ int is_valid(Node* n){
       colum[i] = 0;
       box[i] = 0;
     }
-  }
-  for(int j = 0; j < 9; j++){
-    int num = n->sudo[i][j];
-    if(num != 0){
-      if(fila[num]) return 0;
+    for(int j = 0; j < 9; j++){
+      int num = n->sudo[i][j];
+      if(num != 0){
+        if(fila[num]) return 0;
         fila[num] = 1;
         
         if(colum[num]) return 0;
@@ -64,6 +63,7 @@ int is_valid(Node* n){
         int submatriz = (i / 3) * 3 + (j / 3);
         if (box[submatriz]) return 0;
         box[submatriz] = 1;
+      }
     }
   }
   return 1;
